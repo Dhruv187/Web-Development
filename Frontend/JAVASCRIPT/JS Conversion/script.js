d4382f67dd7f1cce7dc21bc07d4c9793c2e0fc80
Dhruv187 <dhruvgandhi187.com>
2024-07-22 02:13:38 +0530

    JS Conversion

---
+++ Frontend/JAVASCRIPT/JS Conversion/script.js
@@ -0,0 +1,11 @@
+console.log("this file is on conversion of data types");
+
+//Conversion
+
+let variable1 = 12;
+variable1 = String("12");
+console.log(variable1);
+
+let variable2 = true;
+variable2 = Number(true);
+console.log(variable2, typeof variable2);
