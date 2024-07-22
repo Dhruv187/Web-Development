8fdcd912548c7f73009607b9165722f059f06c8e
Dhruv187 <dhruvgandhi187.com>
2024-07-23 02:06:42 +0530

    JS Comparison Operator

---
+++ Frontend/JAVASCRIPT/JS Comparison Operator/script.js
@@ -0,0 +1,53 @@
+//!-- Comparison Operator
+
+//? Equal to
+
+let name1 = "John";
+let name2 = "Steve";
+let userinput = "John";
+console.log(name1 == userinput);
+
+//? Unequal to
+
+let number1 = 45;
+let number2 = 60;
+let input = 45;
+console.log(number1 != input);
+
+//? Strictly equal to
+
+let text1 = "abc";
+let text2 = "xyz";
+let input2 = "abc";
+console.log(text1 === input2);
+
+//? Strictly Unequal to
+
+let text3 = "abc";
+let text4 = "xyz";
+let input3 = "abc";
+console.log(text1 !== input2);
+
+//? Greater
+
+let number3 = 55;
+let number4 = 65;
+console.log(number3 > number4);
+
+//? Lesser
+
+let number5 = 22;
+let number6 = 88;
+console.log(number5 < number6);
+
+//? Greater or equal
+
+let number7 = 65;
+let number8 = 65;
+console.log(number7 >= number8);
+
+//? Lesser or equal
+
+let number9 = 65;
+let number10 = 95;
+console.log(number9 <= number10);
