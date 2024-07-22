e3ebe473e263eccf968da8d98fe6b0b91e508810
Dhruv187 <dhruvgandhi187.com>
2024-07-22 19:08:33 +0530

    Arithmetic Operators

---
+++ Frontend/JAVASCRIPT/JS Arithmetic Operator/script.js
@@ -0,0 +1,62 @@
+//? Arithmetic Operators//
+
+//Addition//
+
+{
+  let a = 5;
+  let b = 6;
+  const result = a + b;
+  console.log(result);
+}
+
+//Subtraction
+
+{
+  let x = 8;
+  let y = 59;
+  const result = x - y;
+  console.log(result);
+}
+
+//Multiplication
+
+{
+  let a = 5;
+  let b = 9;
+  const result = a * b;
+  console.log(result);
+}
+
+//Division
+
+{
+  let x = 200;
+  let y = 10;
+  const result = x / y;
+  console.log(result);
+}
+
+//*To find remainder
+
+{
+  let a = 50;
+  let b = 5;
+  const result = b % a;
+  console.log(result);
+}
+
+// Increment
+
+{
+  let a = 50;
+  a++;
+  console.log(a);
+}
+
+//Decrement
+
+{
+  let b = 56;
+  b--;
+  console.log(b);
+}
