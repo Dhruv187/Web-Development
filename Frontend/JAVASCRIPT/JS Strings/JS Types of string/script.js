33ecb4a936e5094625967d2af2f0ed3d76d39ba0
Dhruv187 <dhruvgandhi187.com>
2024-08-17 02:04:17 +0530

    string concatenation

---
+++ Frontend/JAVASCRIPT/JS Strings/JS Types of string/script.js
@@ -0,0 +1,36 @@
+//Types of string //
+
+//! Single Quote
+let name = "John Wick";
+console.log(name);
+
+//! Double Quote
+let name1 = "Tony Stark";
+console.log(name1);
+
+//! String Constructor
+let new2 = new String("this is a new string");
+console.log(new2);
+
+//? Template Literals
+
+//! Basic Usage
+let code = `Hello World`;
+console.log(code);
+
+//! Expression
+let text = "John";
+let statment = `My name is ${text}`;
+console.log(statment);
+
+//! Multi Line
+const poem = `roses are red
+voilets are blue
+east or west`;
+console.log(poem);
+
+//! Expression Evalutation
+let price = 100;
+let tax = 30;
+
+const total = `price is: ${price * (tax - 10)}`;
