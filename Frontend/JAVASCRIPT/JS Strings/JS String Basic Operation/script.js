1721584e6913d903329e1fa3e102a66c78bed1ca
Dhruv187 <dhruvgandhi187.com>
2024-08-19 00:35:40 +0530

    basic operation

--- Frontend/JAVASCRIPT/JS Strings/JS String Basic Operation/script.js
+++ Frontend/JAVASCRIPT/JS Strings/JS String Basic Operation/script.js
@@ -53,3 +53,47 @@ if (string1.length <= 5) {
 } else {
   console.log("invaild username Retry");
 }
+
+//! Slice Operation
+
+let word6 = "this is my javascript code";
+const sliceword = word6.slice(0, 5);
+console.log(sliceword);
+
+//! Split Operation
+
+//? Split Operation by using Character
+
+let word7 = "Hello, Word";
+const word8 = word7.split(",");
+console.log(word8);
+
+//! IndexOf Operation
+
+let word11 = "Hello World";
+const word12 = word11.indexOf("Hello");
+console.log(word12);
+
+//! String Trim
+
+let word13 = "             Hello World        ";
+const word14 = word13.trim();
+console.log(word14);
+
+//! String Replace
+
+let word15 = "Hello Word";
+const word16 = word15.replace("Word", "World");
+console.log(word16);
+
+//! String LastIndexOf
+
+let word17 = "Hello Word";
+const word18 = word17.lastIndexOf("Hello");
+console.log(word18);
+
+//! String Include
+
+let word19 = "Hello Word";
+const word20 = word19.includes("Hello");
+console.log(word20);
